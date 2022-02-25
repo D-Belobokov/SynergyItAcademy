0cd8298c0fe1b2bc376bd275c2f66a64b62b20c5
D-Belobokov <voyger0781@gmail.com>
2022-02-25 20:28:44 +0300

    Lesson9 2_2 начало

---
+++ src/main/java/Lesson10_2_2/Main.java
@@ -0,0 +1,11 @@
+package Lesson10_2_2;
+
+public class Main {
+    public static void main(String[] args) {
+        String test = new String(" проверка метод spit ");
+        System.out.println(test.length());
+        String[]testArr = test.split("");
+
+
+    }
+}
