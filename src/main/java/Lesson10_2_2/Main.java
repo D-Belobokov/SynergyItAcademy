b39c203603ec0b070c57bc9edc3c63ecead185af
Pilig <voyger0781@gmail.com>
2022-02-28 15:57:22 +0300

    28.02.22

--- src/main/java/Lesson10_2_2/Main.java
+++ src/main/java/Lesson10_2_2/Main.java
@@ -2,10 +2,10 @@ package Lesson10_2_2;
 
 public class Main {
     public static void main(String[] args) {
-        String test = new String(" проверка метод spit ");
-        System.out.println(test.length());
-        String[]testArr = test.split("");
-
-
+        String test = " я куплю тебе змею или черепаху ";
+        String[] words = test.split(" ");
+        for (String word:words){
+            System.out.println(word);
+        }
     }
 }
---
+++ src/main/java/Lesson10_2_2/Sort.java
@@ -0,0 +1,18 @@
+package Lesson10_2_2;
+
+import java.util.Comparator;
+
+public class Sort implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        if(o1.length() > o2.length()){
+            return 1;
+        }else{
+            if(o1.length() < o2.length()){
+                return -1;
+            }else{
+                return 0;
+            }
+        }
+    }
+}
